801238b4c02dceb664e09093699866c8983ea1bf
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-12-08 17:51:08 +0300

    done

--- components/PostDetails.js
+++ components/PostDetails.js
@@ -253,7 +253,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
+    // backgroundColor: "rgba(0,0,0,0.5)",
   },
   modalContainer: {
     backgroundColor: "white",
--- components/Posts.js
+++ components/Posts.js
@@ -190,7 +190,7 @@ const styles = StyleSheet.create({
   modalOverlay: {
     flex: 1,
     justifyContent: "flex-end",
-    backgroundColor: "rgba(0,0,0,0.5)",
+    // backgroundColor: "rgba(0,0,0,0.5)",
   },
 
   modalContainer: {
